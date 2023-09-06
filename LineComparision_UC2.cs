5b4f49b6226fb81d978fdd4d8873f4991593decc
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 18:53:48 +0530

    Add files via upload

---
+++ LineComparision_UC2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class LineComparision_UC2
+{
+    static double Line1()
+    {
+        double x1 = 1.0;
+        double x2 = 2.0;
+        double y1 = 3.0;
+        double y2 = 4.0;
+        double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + (Math.Pow(y2 - y1, 2)));
+        return Line1();
+    }
+    static double Line2()
+    {
+        double x1 = 1.0;
+        double x2 = 2.0;
+        double y1 = 3.0;
+        double y2 = 4.0;
+        double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + (Math.Pow(y2 - y1, 2)));
+        return Line2();
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Line1());
+        Console.WriteLine(Line2());
+    }
+}
+
+
