fd1828b3da96d93c8a8c8f51ee6e191588299d10
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-08 20:15:28 +0530

    Add files via upload

---
+++ LineComparision_UC1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+
+class LineComparision_UC1
+{
+    static double lineLength()
+    {
+        double x1 = 3.0;
+        double y1 = 3.0;
+        double x2 = 4.0;
+        double y2 = 4.0;
+
+        double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + (Math.Pow(y2 - y1, 2)));
+        return length;
+        Console.WriteLine(lineLength());
+
+
+    }
+}
